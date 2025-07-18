0b2ca50f4280001c274f453f22239e2c24ed3543
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2025-07-17 22:14:10 -0400

    Add files via upload

---
+++ src/ui/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer(){
+    return(
+        <div className="container" >
+        <div className="alert alert-dark" role="alert" >2023 - CRM APP</div>
+        </div>
+    );
+
+}
+export default Footer;
