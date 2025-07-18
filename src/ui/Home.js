c63d8ab3f06ac1dc38f6fca624b1ffdc1374f4ee
Matías Fonseca <89324400+Tryton22@users.noreply.github.com>
2025-07-17 22:13:42 -0400

    Add files via upload

---
+++ src/ui/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Home(){
+    return(
+        <>
+        <h1>Bienvenido a la APP</h1>
+        <h1>---------------------</h1>
+    </>
+    );
+
+}
+export default Home;
